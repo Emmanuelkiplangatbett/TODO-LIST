3501040062085c127f1d0849e6b9dff98b0d85ec
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-01-11 08:55:22 +0300

    exported Todoitem component

--- src/component/Todo item .js
+++ src/component/Todo item .js
@@ -2,3 +2,4 @@ import react from "react"
 function Todoitem(){
     return
 }
+export  default Todoitem
--- src/component/Todo list.js
+++ src/component/Todo list.js
@@ -1,4 +1,5 @@
 import react from "react"
+import Todoitem from "./Todo item "
 function Todolist(){
     return
 }
